6b5307a057b94af0e0d1a4f9c3cc585467accae0
Rjcd95 <renejcd95@gmail.com>
2016-07-07 15:00:44 -0600

    Add routing

--- app/app.js
+++ app/app.js
@@ -4,19 +4,30 @@ portfolioPersonal.config(['$routeProvider', function($routeProvider) {
 	
 	$routeProvider
 		.when('/', {
-			templateUrl : '/views/home.html',
+			templateUrl : 'views/home.html',
 			controller  : 'mainController'
 		})
 		.when('/about', {
-			templateUrl : '/views/about.html',
+			templateUrl : 'views/about.html',
 			controller  : 'aboutController'
 		})
 		.when('/contact', {
-			templateUrl : '/views/contact.html',
+			templateUrl : 'views/contact.html',
 			controller  : 'contactController'
 		})
 		.otherwise({
 			redirecTo: '/'
-		});
-		
-}])
+		});	
+}]);
+
+portfolioPersonal.controller('mainController', function($scope) {
+    $scope.message = 'Hola, Mundo!';
+});
+
+portfolioPersonal.controller('aboutController', function($scope) {
+    $scope.message = 'Esta es la página "Acerca de"';
+});
+
+portfolioPersonal.controller('contactController', function($scope) {
+    $scope.message = 'Esta es la página de "Contacto", aquí podemos poner un formulario';
+});
